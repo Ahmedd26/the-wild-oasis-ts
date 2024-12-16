2bb19160d7166402777e28d49630fdcba6a0f89e
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-12-16 02:35:50 +0200

    Installed React-router, initialized routes using routes.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { routes } from "./routes";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
 
-const StyledApp = styled.div`
-    padding: 20px;
-`;
-
-export default function App() {
+function App() {
     return (
         <>
-            <GlobalStyles />
-            <StyledApp>
-                <Heading>The Wild Oasis</Heading>
-            </StyledApp>
+            <GlobalStyles /> {/* Styled Component */}
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate replace to={routes.DASHBOARD} />}
+                    />
+                    <Route path={routes.DASHBOARD} element={<Dashboard />} />
+                    <Route path={routes.BOOKINGS} element={<Bookings />} />
+                    <Route path={routes.CABINS} element={<Cabins />} />
+                    <Route path={routes.USERS} element={<NewUsers />} />
+                    <Route path={routes.SETTINGS} element={<Settings />} />
+                    <Route path={routes.ACCOUNT} element={<Account />} />
+                    <Route path={routes.LOGIN} element={<Login />} />
+                    <Route path={routes.NOT_FOUND} element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
+
+export default App;
---
+++ src/routes.ts
@@ -0,0 +1,23 @@
+// routes.ts
+export const routes = {
+    DASHBOARD: "dashboard" as const,
+    BOOKINGS: "bookings" as const,
+    CABINS: "cabins" as const,
+    USERS: "users" as const,
+    SETTINGS: "settings" as const,
+    ACCOUNT: "account" as const,
+    LOGIN: "login" as const,
+    NOT_FOUND: "*" as const,
+};
+
+/*
+
+APP: {
+        ROOT: "" as const,
+        CITIES: "" as const,
+        CITy: (id: string | number) => `id}` as const,
+        COUNTRIES: "" as const,
+        FORM: "" as const,
+    },
+
+*/
