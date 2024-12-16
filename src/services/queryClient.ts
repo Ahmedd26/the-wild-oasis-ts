c1873f53e6e357841043b0dac3ad84a2c396c6c0
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-12-16 23:46:10 +0200

    modified stale-time for the queryClient

--- src/services/queryClient.ts
+++ src/services/queryClient.ts
@@ -3,7 +3,8 @@ import { QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 60 * 1000, // 1 minute (sec * ms)
+            // staleTime: 60 * 1000, // 1 minute (sec * ms)
+            staleTime: 0,
         },
     },
 });
