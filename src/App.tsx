98c4cab1df2adb96474496b56dd4c6d1c28ca196
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-12-17 16:29:19 +0200

    `Added react-hot-toast library and Toaster component to the application`

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import SpinnerFullPage from "./ui/SpinnerFullPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
     return (
@@ -63,6 +64,26 @@ function App() {
                     </Routes>
                 </Suspense>
             </BrowserRouter>
+            <Toaster
+                position="top-center"
+                gutter={12}
+                containerStyle={{ margin: "8px" }}
+                toastOptions={{
+                    success: {
+                        duration: 3000, // milliseconds
+                    },
+                    error: {
+                        duration: 5000, // milliseconds
+                    },
+                    style: {
+                        fontSize: "16px",
+                        maxWidth: "500px",
+                        padding: "16px 24px",
+                        backgroundColor: "var(--color-grey-0)",
+                        color: "var(--color-grey-700)",
+                    },
+                }}
+            />
         </QueryClientProvider>
     );
 }
