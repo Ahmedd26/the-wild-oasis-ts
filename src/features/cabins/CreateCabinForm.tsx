5b8ebfaccbfc1e9a62886fb73ca6cfda335f80b0
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-12-22 17:49:28 +0200

    feat: wired createCabinForm with react-hook-form and createCabin async fn

--- src/features/cabins/CreateCabinForm.tsx
+++ src/features/cabins/CreateCabinForm.tsx
@@ -5,6 +5,12 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import { FieldValues, useForm } from "react-hook-form";
+import { ICabin } from "../../types/cabin.interface";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import { queryKeys } from "../../types/queryKey";
 
 const FormRow = styled.div`
     display: grid;
@@ -43,45 +49,71 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
+    const queryClient = useQueryClient();
+    const { register, handleSubmit, reset } = useForm<ICabin>();
+    const { mutate, isLoading: isCreating } = useMutation({
+        mutationFn: createCabin,
+        onSuccess: (data) => {
+            toast.success("New cabin successfully created");
+            queryClient.invalidateQueries({ queryKey: [queryKeys.CABINS] });
+            reset();
+        },
+        onError: (error: Error) => toast.error(error.message),
+    });
+
+    function onSubmit(data: ICabin) {
+        mutate(data);
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
             <FormRow>
                 <Label htmlFor="name">Cabin name</Label>
-                <Input type="text" id="name" />
+                <Input type="text" id="name" {...register("name")} />
             </FormRow>
-
             <FormRow>
                 <Label htmlFor="maxCapacity">Maximum capacity</Label>
-                <Input type="number" id="maxCapacity" />
+                <Input
+                    type="number"
+                    id="maxCapacity"
+                    {...register("maxCapacity")}
+                />
             </FormRow>
-
             <FormRow>
                 <Label htmlFor="regularPrice">Regular price</Label>
-                <Input type="number" id="regularPrice" />
+                <Input
+                    type="number"
+                    id="regularPrice"
+                    {...register("regularPrice")}
+                />
             </FormRow>
-
             <FormRow>
                 <Label htmlFor="discount">Discount</Label>
-                <Input type="number" id="discount" defaultValue={0} />
+                <Input
+                    type="number"
+                    id="discount"
+                    defaultValue={0}
+                    {...register("discount")}
+                />
             </FormRow>
-
             <FormRow>
                 <Label htmlFor="description">Description for website</Label>
-                <Textarea id="description" defaultValue="" />
+                <Textarea
+                    id="description"
+                    defaultValue=""
+                    {...register("description")}
+                />
                 {/* <Textarea type="number" id="description" defaultValue="" /> */}
             </FormRow>
-
             <FormRow>
                 <Label htmlFor="image">Cabin photo</Label>
-                <FileInput id="image" accept="image/*" />
+                <FileInput id="image" accept="image/*" {...register("image")} />
             </FormRow>
-
             <FormRow>
                 {/* type is an HTML attribute! */}
                 <Button variation="secondary" type="reset">
                     Cancel
                 </Button>
-                <Button>Edit cabin</Button>
+                <Button disabled={isCreating}>Add cabin</Button>
             </FormRow>
         </Form>
     );
