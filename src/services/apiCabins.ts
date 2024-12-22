5f43f11d2d0738f4ffc24d9304d757e95c563a50
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-12-22 17:48:42 +0200

    feat: added createCabin async function

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,8 +1,10 @@
+import { ICabin, ICabinRes } from "../types/cabin.interface";
+import { queryKeys } from "../types/queryKey";
 import supabase from "./supabase";
 
 // Get All Cabins
 export async function getCabins() {
-    const { data, error } = await supabase.from("cabins").select("*");
+    const { data, error } = await supabase.from(queryKeys.CABINS).select("*");
     if (error) {
         console.error(error);
         throw new Error("Cabins could not be retrieved");
@@ -10,9 +12,23 @@ export async function getCabins() {
     return data;
 }
 
+export async function createCabin(newCabin: ICabin) {
+    // const { data, error }: { data: ICabinRes[] | null, error: Error | null } = await supabase.from("cabins").insert([newCabin]);
+    const { data, error } = await supabase
+        .from(queryKeys.CABINS)
+        .insert([newCabin]);
+
+    if (error) {
+        console.error(error);
+        throw new Error("Cabin could not be created");
+    }
+
+    return data;
+}
+
 export async function deleteCabin(cabinId: number) {
     const { data, error } = await supabase
-        .from("cabins")
+        .from(queryKeys.CABINS)
         .delete()
         .eq("id", cabinId);
     if (error) {
