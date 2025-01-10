6c6da475481d3d7e833019b77ad3efd03f17e209
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-01-10 23:10:39 +0200

    `feat: added Duration Pie Chart component and updated DashboardLayout to include it`

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -32,7 +33,7 @@ function DashboardLayout() {
                 cabinCount={Number(cabins?.length) || 0}
             />
             <div>Today's Activity</div>
-            <div>Chart: stay durations</div>
+            <DurationChart confirmedStays={confirmedStays || []} />
             <SalesChart bookings={bookings || []} numDays={numDays} />
         </StyledDashboardLayout>
     );
